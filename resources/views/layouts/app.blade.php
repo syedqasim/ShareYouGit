9cdf131537aeefc42508e4acf6b8091f722ca17f
Qasim <syedqasim@live.com>
2018-02-04 09:43:22 +0400

    layout and index page styling also navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}" >
         <title>{{config('app.name','ShareYou')}}</title>
     </head>
     <body>
-      @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
